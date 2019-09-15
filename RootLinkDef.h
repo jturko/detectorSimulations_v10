e157e1a9ab18cf64d1c87454396d18d1c8a99420
jturko <jturko@mail.uoguelph.ca>
2019-09-15 16:36:51 +0800

    Adding ParticleMC w/ ability to be save to ROOT file

--- RootLinkDef.h
+++ RootLinkDef.h
@@ -3,4 +3,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class TRexSettings+;
+#pragma link C++ class ParticleMC+;
+#pragma link C++ class vector<ParticleMC>+;
 #endif
