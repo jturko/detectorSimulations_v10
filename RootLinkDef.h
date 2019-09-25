74b65c809533f0da984943ca522feec58b6c33e2
jturko <jturko@mail.uoguelph.ca>
2019-09-25 17:16:33 +0800

    forgot to include these in the last commit

--- RootLinkDef.h
+++ RootLinkDef.h
@@ -2,7 +2,7 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class TRexSettings+;
+#pragma link C++ class TistarSettings+;
 #pragma link C++ class ParticleMC+;
 #pragma link C++ class vector<ParticleMC>+;
 #endif
