5c134c23c08c8103b57b4c2cf50adeb0875d4e9c
jturko-linuxmint <joey.turko@gmail.com>
2020-05-18 15:16:54 -0400

    modifications to method of how batch/interactive modes are started

--- Griffinv10.cc
+++ Griffinv10.cc
@@ -46,14 +46,8 @@
 #endif
 
 #include "G4UImanager.hh"
-
-#ifdef G4VIS_USE
 #include "G4VisExecutive.hh"
-#endif
-
-#ifdef G4UI_USE
 #include "G4UIExecutive.hh"
-#endif
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -64,6 +58,11 @@ int main(int argc, char** argv)
 	 // G4long is at least 32 bits. 
     G4long seed = time(NULL);
     G4Random::setTheSeed(seed);
+
+    G4UIExecutive* ui = 0;
+    if ( argc == 1 ) {
+    	ui = new G4UIExecutive(argc, argv);
+    }
 
     // Construct the default run manager
 //#ifdef G4MULTITHREADED
@@ -79,49 +78,40 @@ int main(int argc, char** argv)
 	 G4RunManager* runManager = new G4RunManager;
 //#endif
 
-	 // Set mandatory initialization classes
-	 DetectorConstruction* detector = new DetectorConstruction;
-	 runManager->SetUserInitialization(detector);
-    	
-	 runManager->SetUserInitialization(new PhysicsList);
-	
-     runManager->SetUserInitialization(new ActionInitialization(detector));
-
-	 // We don't initialize the G4 kernel at run time so the physics list can be changed!
-
-	 // Get the pointer to the User Interface manager
-	 G4UImanager* UImanager = G4UImanager::GetUIpointer();
-
-#ifdef G4VIS_USE
-	 G4VisManager* visManager = new G4VisExecutive;
-	 visManager->Initialize();
-#endif
-
-	 if(argc != 1) { // batch mode
-		 G4String command = "/control/execute ";
-		 G4String fileName = argv[1];
-		 UImanager->ApplyCommand(command+fileName);
-	 } else { // interactive mode : define visualization and UI terminal
-#ifdef G4UI_USE
-		 G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-#endif
-		 UImanager->ApplyCommand("/control/execute vis.mac");
-
-#ifdef G4UI_USE
-		 ui->SessionStart();
-
-		 delete ui;
-#endif
-	 }
-
-#ifdef G4VIS_USE
-		 delete visManager;
-#endif
-
-	 // Job termination
-	 delete runManager;
-
-	 return 0;
+    // Set mandatory initialization classes
+    DetectorConstruction* detector = new DetectorConstruction;
+    runManager->SetUserInitialization(detector);   
+    runManager->SetUserInitialization(new PhysicsList);
+    runManager->SetUserInitialization(new ActionInitialization(detector));
+    
+    // We don't initialize the G4 kernel at run time so the physics list can be changed!
+    
+    // Initialize visualization
+    //
+    G4VisManager* visManager = new G4VisExecutive;
+    visManager->Initialize();
+    
+    // Get the pointer to the User Interface manager
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+    
+    // Process macro or start UI session
+    //
+    if ( ! ui ) {
+        // batch mode
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
+    }
+    else {
+        // interactive mode
+        ui->SessionStart();
+        delete ui;
+    }
+    
+    delete visManager;
+    delete runManager;
+    
+    return 0;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
